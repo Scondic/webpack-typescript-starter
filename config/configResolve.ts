188bcd52d98f2143666205f76be7608fea39a009
vadimterentev <scondic@icloud.com>
2023-01-23 09:49:53 +0300

    Added support for aliases

--- config/configResolve.ts
+++ config/configResolve.ts
@@ -1,10 +1,13 @@
 import webpack from "webpack";
 import type { WebpackBuildOptions } from "./types";
 
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
 export function configResolve(
   options: WebpackBuildOptions
 ): webpack.ResolveOptions {
   return {
     extensions: [".tsx", ".ts", ".js"],
+    plugins: [new TsconfigPathsPlugin()],
   };
 }
